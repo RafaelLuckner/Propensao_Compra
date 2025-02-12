2538b938729c3b7044e8228586c99c54e099b47c
RafaelLuckner <rafaelluckner1@gmail.com>
2025-02-12 15:24:43 -0300

    att porta da api

--- api/handler.py
+++ api/handler.py
@@ -1,11 +1,11 @@
-from propensityscore.PropensityScore import PropensityScore
 import os
+from propensityscore.PropensityScore import PropensityScore
 import json
 import pickle
 import pandas as pd
-from flask import Flask, request, Response
+from flask import Flask, request, jsonify  # Importa jsonify corretamente
 
-# loading model 
+# Carregando o modelo
 model = pickle.load(open('models/rf.pkl', 'rb'))
 
 app = Flask(__name__)
@@ -14,26 +14,22 @@ app = Flask(__name__)
 def predict():
     test_json = request.get_json()
 
-
-    if test_json: # there is data
-        if isinstance(test_json, dict):  # unique instance
+    if test_json:  # Se houver dados
+        if isinstance(test_json, dict):  # Caso seja uma única instância
             raw_data = pd.DataFrame(test_json, index=[0])
-
-        else: # multiple instances
+        else:  # Caso sejam múltiplas instâncias
             raw_data = pd.DataFrame(json.loads(test_json))
 
-
+        # Pipeline de transformação
         pipeline = PropensityScore(raw_data)
         pipeline.rename_cols()
         pipeline.transform()
-        predict = pipeline.predict(model)
-        
-        return predict
+        prediction = pipeline.predict(model)
 
-    else: 
-        return Response('{}', status=200, mimetype='application/json')
+        return jsonify({"prediction": prediction})  # Agora correto
 
-    
+    return jsonify({}), 400  # Resposta vazia com código de erro
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Render define a porta dinamicamente
+    app.run(host='0.0.0.0', port=port)
